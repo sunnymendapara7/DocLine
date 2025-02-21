f1691d1e84c488534f0bae340c01e50cca4271ce
sunnymendapara7 <sunnymendapara09@gmail.com>
2025-02-21 21:38:51 +0530

    folder structure and requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DocLine',
+    version= '0.0.0',
+    author= 'Sunny Mendapara',
+    author_email= 'sunnymendapara09@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
